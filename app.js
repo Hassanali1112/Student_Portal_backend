b1fbc7d05ab0dcce1333320167cb0a50dcd371a8
Hassanali1112 <134091195+Hassanali1112@users.noreply.github.com>
2025-06-30 04:35:48 +0500

    app listener updated

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use('/api/auth',authRoutes)
 
 app.use('/api/applications', applicationsRouter)
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
