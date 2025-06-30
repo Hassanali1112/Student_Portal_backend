b88aca2ca295e6c8ed53b51e2f95e58a258966e3
Hassanali1112 <134091195+Hassanali1112@users.noreply.github.com>
2025-06-30 06:15:49 +0500

    testing for deployment

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const app = express();
 
 app.use(cors())
 app.use(express.json())
+app.use((req, res) => {
+  console.log("⚠️  Unknown route hit:", req.originalUrl);
+  res.status(404).send("Route not found");
+});
 
 const PORT= process.env.PORT || 8080
 
@@ -15,16 +19,18 @@ mongoose.connection.on("open", () => {
   console.log(`MongoDB Connected`);
 });
 
-app.get("/", (req, res)=>{
-  console.log("here is server")
-  res.send("hello from server")
-})
+app.get("/", (req, res) => {
+  console.log("⚡ Base route hit");
+  res.status(200).send("hello from server");
+});
+
 
 app.use('/api/auth',authRoutes)
 
 app.use('/api/applications', applicationsRouter)
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`✅ App is running and listening on actual port: ${PORT}`);
 });
+
 
